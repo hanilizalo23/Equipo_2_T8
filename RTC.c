2b5b496e45a21cab22b1092329f135508ece7f36
Mauricio Peralta O <mperalta.osorio@iteso.mx>
2022-03-23 14:42:39 -0600

    fuction set_12_or_24 added

--- RTC.c
+++ RTC.c
@@ -5,4 +5,20 @@
  *      Author: Mauricio Peralta
  */
 
+#include "RTC.h"
+#include "I2C.h"
+
+void set_12_or_24(is_12_or_24_t select)
+{
+	uint8_t temp = 0;
+	temp = i2c_master_read(HOUR_ADDRESS);
+	if(select)
+	{
+		i2c_master_write(HOUR_ADDRESS,(temp | SET_12));
+	}
+	else
+	{
+		i2c_master_write(HOUR_ADDRESS,(temp & SET_24));
+	}
+}
 
--- RTC.h
+++ RTC.h
@@ -1,5 +1,5 @@
 /*
- * RTC.h
+ * RTC.c
  *
  *  Created on: 22 mar 2022
  *      Author: Mauricio Peralta
@@ -8,6 +8,30 @@
 #ifndef RTC_H_
 #define RTC_H_
 
+#include "I2C.h"
+
+#define SEC_ADDRESS 0x00U
+#define MIN_ADDRESS 0x01U
+#define HOUR_ADDRESS 0x02U
+#define HOUR_READ_MASK (0x1FU)
+#define AM_SET (0x20U)
+#define PM_SET (0XDFU)
+#define SET_24 (0xBFU)
+#define SET_12 (0x40U)
+#define CLEAR_HOUR (0x60U)
+
+typedef enum {
+	TFOUR,
+	TWELVE
+} is_12_or_24_t;
+
+
+typedef enum {
+	AM,
+	PM
+} AM_or_PM_t;
+
+void set_12_or_24(is_12_or_24_t select); /**Setter for hour format*/
 
 
 #endif /* RTC_H_ */
