5d450a9656a9062005dc0bcfd0e7a2860340efcf
Mauricio Peralta O <mperalta.osorio@iteso.mx>
2022-03-22 22:08:59 -0600

    first commit of I2C

---
+++ I2C_driver.c
@@ -0,0 +1,6 @@
+/*
+ * I2C_driver.c
+ *
+ *  Created on: 22 mar 2022
+ *      Author: Mauricio Peralta
+ */
---
+++ I2C_driver.h
@@ -0,0 +1,13 @@
+/*
+ * I2C_driver.h
+ *
+ *  Created on: 22 mar 2022
+ *      Author: Mauricio Peralta
+ */
+
+#ifndef I2C_DRIVER_H_
+#define I2C_DRIVER_H_
+
+
+
+#endif /* I2C_DRIVER_H_ */
