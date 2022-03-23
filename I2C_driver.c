7b89ce53e122de573fb43d6f8bfde7f7e156e8a2
Mauricio Peralta O <mperalta.osorio@iteso.mx>
2022-03-23 14:45:25 -0600

    initialitation added

--- I2C_driver.c
+++ I2C_driver.c
@@ -4,3 +4,29 @@
  *  Created on: 22 mar 2022
  *      Author: Mauricio Peralta
  */
+
+#include "I2C.h"
+
+uint8_t g_master_txBuff[I2C_DATA_LENGTH];
+uint8_t g_master_rxBuff[I2C_DATA_LENGTH];
+i2c_master_config_t g_masterConfig;
+uint32_t g_sourceClock;
+i2c_master_transfer_t g_masterXfer;
+
+void i2c_master_init(void)
+{
+	GPIO_clock_gating(GPIO_B);
+	gpio_pin_control_register_t i2c_pin_config = GPIO_MUX2 | GPIO_PE;
+	GPIO_pin_control_register(GPIO_B,bit_2,  &i2c_pin_config); /**SCL*/
+	GPIO_data_direction_pin(GPIO_B, GPIO_OUTPUT,  bit_2);
+	GPIO_pin_control_register(GPIO_B,bit_3,  &i2c_pin_config); /**SDA*/
+	GPIO_data_direction_pin(GPIO_B, GPIO_OUTPUT,  bit_3);
+    BOARD_BootClockRUN();
+
+	I2C_MasterGetDefaultConfig(&g_masterConfig);
+	g_masterConfig.baudRate_Bps = I2C_BAUDRATE;
+	g_sourceClock = I2C_MASTER_CLK_FREQ;
+	I2C_MasterInit(EXAMPLE_I2C_MASTER_BASEADDR, &g_masterConfig, g_sourceClock);
+}
+
+
--- I2C_driver.h
+++ I2C_driver.h
@@ -5,9 +5,26 @@
  *      Author: Mauricio Peralta
  */
 
-#ifndef I2C_DRIVER_H_
-#define I2C_DRIVER_H_
+#ifndef I2C_H_
+#define I2C_H_
+
+#include <string.h>
+#include "pin_mux.h"
+#include "board.h"
+#include "fsl_debug_console.h"
+#include "fsl_i2c.h"
+#include "GPIO.h"
+#include "Bits.h"
+
+#define I2C_MASTER_CLK_SRC          I2C0_CLK_SRC
+#define I2C_MASTER_CLK_FREQ         CLOCK_GetFreq(I2C0_CLK_SRC)
+#define EXAMPLE_I2C_MASTER_BASEADDR I2C0
+
+#define I2C_MASTER_SLAVE_ADDR_7BIT 0x6FU
+#define I2C_BAUDRATE               100000U
+#define I2C_DATA_LENGTH            1U
+
+void i2c_master_init(void); /**Initialize*/
 
 
-
-#endif /* I2C_DRIVER_H_ */
+#endif /* I2C_H_ */
