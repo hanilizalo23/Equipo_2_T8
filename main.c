26cd89b60664993deb69d35dcdba06826ed4f6fe
hanilizalo23 <nelida_phm@hotmail.com>
2022-03-23 00:33:53 -0600

    Main with everything until UART

--- main.c
+++ main.c
@@ -3,3 +3,22 @@
  *  Created on: 21 mar 2022
  *      Author: Nelida Hernández & Mauricio Peralta
  */
+
+#include "GPIO.h"
+#include "Bits.h"
+#include "I2C_driver.h"
+#include "RTC.h"
+#include "UART_driver.h"
+
+int main (void)
+{
+	BOARD_InitDebugConsole();
+	UART_init();
+	UART_loop();
+
+	while (1)
+	{
+
+	}
+    return 0;
+}
