9a122f526b2f0eeccf2d6cb1b36b0778671428f4
hanilizalo23 <nelida_phm@hotmail.com>
2022-03-23 23:39:23 -0600

    Some features added

--- UART_driver.c
+++ UART_driver.c
@@ -20,9 +20,20 @@
 #define KEY_ONE 49
 #define KEY_TWO 50
 #define KEY_ESC 27
+#define ECHO_BUFFER_LENGTH 8
 
 typedef enum{MAIN_MENU, CLOCK, SET_CLOCK} function_t;
 typedef enum{OFF, ON} functionality_state_t;
+
+uart_handle_t g_uartHandle;
+uint8_t g_tipString[] ="\033[0;34;43m";
+uint8_t g_tipString2[]="ARD";
+uint8_t g_txBuffer[ECHO_BUFFER_LENGTH] = {0};
+uint8_t g_rxBuffer[ECHO_BUFFER_LENGTH] = {0};
+volatile bool rxBufferEmpty            = true;
+volatile bool txBufferFull             = false;
+volatile bool txOnGoing                = false;
+volatile bool rxOnGoing                = false;
 
 void UART_init(void)
 {
@@ -34,7 +45,6 @@ void UART_init(void)
     config.enableRx     = true;
 
     UART_Init(DEMO_UART, &config, DEMO_UART_CLK_FREQ);
-
 	CLOCK_EnableClock(kCLOCK_PortB);
 
 	PORT_SetPinMux(PORTB, 16U, kPORT_MuxAlt3);
@@ -57,13 +67,14 @@ void UART_loop(void)
         {
         	if(TRUE == functionality_state)
         	{
-        		UART_WriteBlocking(DEMO_UART, "\033[0;35;43m", sizeof("\033[0;35;43m") - 1);
+        		UART_WriteBlocking(DEMO_UART, g_tipString, sizeof(g_tipString) - 1);
 				UART_WriteBlocking(DEMO_UART, "\033[2J", sizeof("\033[2J") - 1);
 				UART_WriteBlocking(DEMO_UART, "\033[10;10H", sizeof("\033[10;10H") - 1);
 				UART_WriteBlocking(DEMO_UART, "1) Leer Hora", sizeof("1) Leer Hora") - 1);
 				UART_WriteBlocking(DEMO_UART, "\033[11;10H", sizeof("\033[11;10H") - 1);
 				UART_WriteBlocking(DEMO_UART, "2) Configurar Hora", sizeof("2) Configurar Hora") - 1);
 				UART_WriteBlocking(DEMO_UART, "\033[12;10H", sizeof("\033[12;10H") - 1);
+
 				functionality_state = FALSE;
         	}
         	if(KEY_ONE == key_pressed)
@@ -81,13 +92,14 @@ void UART_loop(void)
 		{
         	if(TRUE == functionality_state)
 			{
-        		UART_WriteBlocking(DEMO_UART, "\033[0;35;43m", sizeof("\033[0;35;43m") - 1);
+        		UART_WriteBlocking(DEMO_UART, "\033[0;34;43m", sizeof("\033[0;34;43m") - 1);
         		UART_WriteBlocking(DEMO_UART, "\033[2J", sizeof("\033[2J") - 1);
         		UART_WriteBlocking(DEMO_UART, "\033[10;10H", sizeof("\033[10;10H") - 1);
 				UART_WriteBlocking(DEMO_UART, "1) Leer Hora", sizeof("1) Leer Hora") - 1);
 				UART_WriteBlocking(DEMO_UART, "\033[11;10H", sizeof("\033[11;10H") - 1);
 				UART_WriteBlocking(DEMO_UART, "  2) 12:35:20 AM", sizeof("  2) 12:35:20 AM") - 1);
 				UART_WriteBlocking(DEMO_UART, "\033[12;10H", sizeof("\033[12;10H") - 1);
+
 				functionality_state = FALSE;
 			}
 			else
@@ -104,11 +116,13 @@ void UART_loop(void)
 		{
         	if(TRUE == functionality_state)
 			{
-        		UART_WriteBlocking(DEMO_UART, "\033[0;35;43m", sizeof("\033[0;35;43m") - 1);
+        		UART_WriteBlocking(DEMO_UART, "\033[0;34;43m", sizeof("\033[0;34;43m") - 1);
         		UART_WriteBlocking(DEMO_UART, "\033[2J", sizeof("\033[2J") - 1);
         		UART_WriteBlocking(DEMO_UART, "\033[10;10H", sizeof("\033[10;10H") - 1);
 				UART_WriteBlocking(DEMO_UART, "1) Introducir la hora actual en formato HH:MM:SS", sizeof("1) Introducir la hora actual en formato HH:MM:SS") - 1);
 				UART_WriteBlocking(DEMO_UART, "\033[11;10H", sizeof("\033[11;10H") - 1);
+				UART_ReadBlocking ( DEMO_UART , g_tipString2, sizeof(g_tipString2) - 1 );
+				UART_WriteBlocking(DEMO_UART, g_tipString, sizeof(g_tipString) - 1);
 				functionality_state = FALSE;
 			}
 			else
@@ -122,5 +136,22 @@ void UART_loop(void)
 			}
 		}
         UART_ReadBlocking(DEMO_UART, &key_pressed, 1);
+    }
+
+void UART_UserCallback(UART_Type *base, uart_handle_t *handle, status_t status, void *userData)
+    {
+        userData = userData;
+
+        if (kStatus_UART_TxIdle == status)
+        {
+            txBufferFull = false;
+            txOnGoing    = false;
+        }
+
+        if (kStatus_UART_RxIdle == status)
+        {
+            rxBufferEmpty = false;
+            rxOnGoing     = false;
+        }
     }
 }
--- UART_driver.h
+++ UART_driver.h
@@ -5,11 +5,13 @@
  *      Author: Nelida Hernández
  */
 
+#include "fsl_uart.h"
 
 #ifndef UART_H_
 #define UART_H_
 
 void UART_init(void); /**Function who initialize everything**/
 void UART_loop(void); /**Function for configure the menu and refresh it**/
+void UART_UserCallback(UART_Type *base, uart_handle_t *handle, status_t status, void *userData); /**Callback that might help, from SDK*/
 
 #endif /* UART_H_ */
